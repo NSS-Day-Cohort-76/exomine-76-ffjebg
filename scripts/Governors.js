b9bc7e070e7e743dfbd927052b31103005c3fca3
Jake Littleton <jakelittleton30@gmail.com>
2025-04-11 15:10:26 -0500

    new branch

---
+++ scripts/Governors.js
@@ -0,0 +1,7 @@
+export const GovernorsList = () => {
+    const Governors = getGovernors()
+    return `${Governors.map(gov => `
+        <div>
+        <option value="${gov.id}">${gov.name}</option>`).join("")}
+        </div>`
+}
