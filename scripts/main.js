03dbc9e469bd6f6a47e589978b68a477e5a87628
charlie-laptop <cww324@gmail.com>
2025-04-14 12:00:58 -0500

    took out a part of index.html to use in the Button.js or main.js file and added a dom handler to main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,16 @@
-import { render } from "./Exomine.js"
+import { render } from "./Exomine.js";
+import { CompleteButton, placeOrder } from "./Button.js";
 
 const renderAllHTML = async () => {
-    await render()
-}
-document.addEventListener("newOrderCreated", event => {
-    console.log("State of data has changed. Regenerating HTML...")
-    renderAllHTML()
-})
+  await render();
+};
+
+// changed newOrderCreated to stateChanged
+document.addEventListener("stateChanged", (event) => {
+  console.log("State of data has changed. Regenerating HTML...");
+  renderAllHTML();
+});
+
+// adding the Purchase button to the DOM right hurr and the click listener
+document.querySelector("#spaceCart").innerHTML += CompleteButton();
+placeOrder();
