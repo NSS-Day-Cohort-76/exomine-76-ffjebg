5df8b70e973c22f70849ad44791c67034caf07d4
Brit Beaty <britneybeaty@gmail.com>
2025-04-14 09:03:03 -0500

    updated MiningFacility module

--- scripts/MiningFacility.js
+++ scripts/MiningFacility.js
@@ -1,14 +1,27 @@
 import { getFacilities } from "./TransientState.js"
 
-export const miningFacilities = () => {
- const facilities = getFacilities()
+const setMiningFacilityId = (changeEvent) => {
+    if (changeEvent.target.name === "facility") {
+        const miningFacilityId = parseInt(changeEvent.target.value)
+        setMiningFacilityId(miningFacilityId)
+    }
 
-            return `${facilities.map(  =>
-            `<div>
-                    <select id="facility">
-                        <option value="${facility.id}">${facility.title} </option>
-                     </select>
-            </div>`).join(" ")
 }
 
+export const miningFacilities = () => {
+ const facilities = getFacilities()
+document.addEventListener("change", setMiningFacilityId)
 
+let html = `<select id="facility">`
+html += `<option value="0">Choose a facility</option>`
+
+ const facilitiesArray = facilities.map(
+    (facility) => {       
+        return `<option value="${facility.id}">${facility.title} </option>`
+        }    
+    )
+
+    html += facilitiesArray.join(" ")
+    html += "</select>"
+    return html
+}
