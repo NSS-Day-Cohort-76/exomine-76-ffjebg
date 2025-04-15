58fe0cbce83798d3309ff17f0eaf5b205ebf6fa5
Brit Beaty <britneybeaty@gmail.com>
2025-04-15 10:58:04 -0500

    added colony Mineral module

--- scripts/ColonyMinerals.js
+++ scripts/ColonyMinerals.js
@@ -1,33 +1,31 @@
 
-import { getColonyMinerals, getGovernors, getMinerals, getColonies } from "./TransientState.js"
+import { getColonyMinerals, getGovernors, getMinerals, getColonies, setGovernor } from "./TransientState.js"
 
 
 
-export const ColonyMineralsList = () => {
+export const ColonyMineralsList =  () => {
    const ColonyMinerals = getColonyMinerals()
    const Minerals = getMinerals()
    const Governors = getGovernors()
    const Colonies = getColonies()
 
-   const selectedGovernor = Governors
+   const selectedGovernor = setGovernor()
    let html = '<div>'
 
    if (selectedGovernor) {
-      const governorColonyId = selectedGovernor.colonyId
-
-
-      const colony = Colonies.find(colony => colony.id === governorColonyId)
+      const selectedGovernor = Governors.find(gov => gov.id === selectedGovernor.id)
+      const colony = Colonies.find(colony => colony.id === selectedGovernor.colonyId)
       html += `<h2>${colony.title} Minerals</h2>`
 
-      const colonyMineralData = ColonyMinerals.filter(colonyMineral => ColonyMinerals.mineralId)
+      const colonyMineralData = ColonyMinerals.filter(colonyMineral => colonyMineral.mineralId)
 
       for (const colonyMineral of colonyMineralData){
          const mineral = Minerals.find(mineral => mineral.id === colonyMineral.MineralId)
          html += `<p>${colonyMineral.quantity} tons of ${mineral.name}</p>`
       }
-
+ 
    } else {
-      html += "<h2>Colony Minerals</h2>"
+      html += "<h2><strong>Colony Minerals</strong></h2>"
    }
 
    html += '</div>'
